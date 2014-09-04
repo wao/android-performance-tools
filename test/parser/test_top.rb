6592a513f4e0298108258f084b8e38301223ab72
Yang Chen <mike.c.yang@gmail.com>
2014-09-04 11:47:56 +0800

    VIM auto commit:Thu 04 Sep 2014 11:47:56 AM CST    command:!testrb test/parser/test_top.rb Run options: -Ilib:test
    # Running tests:
    
    727484K
    used
    12848K
    free
    0K
    shrd
    27448K
    buff
    195288K
    cached
    ..
    
    Finished tests in 0.003383s, 591.2512 tests/s, 4138.7584 assertions/s.
    
    2 tests, 14 assertions, 0 failures, 0 errors, 0 skips
    
    ruby -v: ruby 2.1.2p95 (2014-05-08 revision 45877) [x86_64-linux]

--- test/parser/test_top.rb
+++ test/parser/test_top.rb
@@ -9,6 +9,11 @@ class TestParserTop < Test::Unit::TestCase
 Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached
 CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq
 Load average: 2.98 2.96 1.78 1/907 3607
+  PID  PPID USER     STAT   VSZ %VSZ CPU %CPU COMMAND
+ 1120  1106 0        S    1265m175.0   0  2.7 /applications/bin/tvos
+ 1099     1 1013     S     279m 38.6   0  2.7 {OPEN} /system/bin/mediaserver
+ 1096     1 1000     S     154m 21.3   0  2.7 {Binder_2} /system/bin/surfaceflinger
+ 1148     1 0        S     2660  0.3   3  1.3 /system/bin/logcat -b system -b events -b main -b radio -K -n 20 -r 5000 -v threadtime -f /data/Logs/Log.0/logcat.log
 __DATA__
         @io = StringIO.new sample 
     end
@@ -23,6 +28,11 @@ __DATA__
         assert_equal( 'CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq', top.get_line )
         assert_equal( 'Load average: 2.98 2.96 1.78 1/907 3607', top.peek_line )
         assert_equal( 'Load average: 2.98 2.96 1.78 1/907 3607', top.get_line )
+        top.get_line
+        top.get_line
+        top.get_line
+        top.get_line
+        top.get_line
         assert_nil top.peek_line
         assert_nil top.get_line
     end
@@ -38,6 +48,5 @@ __DATA__
         assert_equal "0K", mem["shrd"]
         assert_equal "27448K", mem["buff"]
         assert_equal "195288K", mem["cached"]
-
     end
 end
