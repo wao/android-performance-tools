be713f9be50aa240d7cf8a656811f66b73867ac7
Yang Chen <mike.c.yang@gmail.com>
2014-09-04 11:43:12 +0800

    VIM auto commit:Thu 04 Sep 2014 11:43:12 AM CST    command:!testrb test/parser/test_top.rb Run options: -Ilib:test
    # Running tests:
    
    E.
    
    Finished tests in 0.003660s, 546.3915 tests/s, 2185.5660 assertions/s.
    
      1) Error:
    TestParserTop#test_mem:
    NameError: undefined local variable or method `top' for #<TestParserTop:0x000000022e3d10>
        /home/w19816/myprojects/android-performance-tools/test/parser/test_top.rb:32:in `test_mem'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1265:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit/testcase.rb:17:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:940:in `block in _run_suite'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:933:in `map'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:933:in `_run_suite'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:663:in `block in _run_suites'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:661:in `each'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:661:in `_run_suites'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:884:in `_run_anything'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1092:in `run_tests'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1079:in `block in _run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1078:in `each'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1078:in `_run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1066:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:27:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:780:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:840:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:844:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/bin/testrb:3:in `<main>'
    
    2 tests, 8 assertions, 0 failures, 1 errors, 0 skips
    
    ruby -v: ruby 2.1.2p95 (2014-05-08 revision 45877) [x86_64-linux]

--- test/parser/test_top.rb
+++ test/parser/test_top.rb
@@ -4,15 +4,18 @@ require File.dirname(__FILE__) + "/../test_helper.rb"
 require 'parser/top'
 
 class TestParserTop < Test::Unit::TestCase
-    def test_peek_and_getline
+    def setup
         sample = <<__DATA__
 Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached
 CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq
 Load average: 2.98 2.96 1.78 1/907 3607
 __DATA__
-        io = StringIO.new sample 
+        @io = StringIO.new sample 
+    end
 
-        top = Parser::Top.new(io)
+    def test_peek_and_getline
+
+        top = Parser::Top.new(@io)
 
         assert_equal( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
         assert_equal( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
@@ -22,5 +25,16 @@ __DATA__
         assert_equal( 'Load average: 2.98 2.96 1.78 1/907 3607', top.get_line )
         assert_nil top.peek_line
         assert_nil top.get_line
+    end
+
+    def test_mem
+        mem = nil
+        assert_nothing_thrown( mem = top.expect_mem )
+        assert_equal "727484K", mem["used"]
+        assert_equal "12848K", mem["free"]
+        assert_equal "0K", mem["shrd"]
+        assert_equal "27448K", mem["buff"]
+        assert_equal "195288K", mem["cached"]
+
     end
 end
