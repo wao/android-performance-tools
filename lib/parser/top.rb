e9eae2b7d46cb360f6fc4a2e47849e4ed6f84f6f
Yang Chen <mike.c.yang@gmail.com>
2014-09-04 11:27:18 +0800

    Initialize and implement top

---
+++ lib/parser/top.rb
@@ -0,0 +1,107 @@
+module Parser
+    # Parser busybox top -b output and put it into a list
+    class Top
+        Load = Struct.new( :one, :five, :fifteen )
+        Sample = Struct.new(:mem, :cpu, :load, :pids)
+
+        def initialize(io)
+            @io = io
+            @peek_line = nil
+        end
+
+        def get_line
+            if @peek_line
+                ret = @peek_line
+                @peek_line = nil
+            else
+                ret = @io.readline.strip
+            end
+
+            ret
+        end
+
+        def peek_line
+            if @peek_line.nil?
+                @peek_line = @io.readline.strip
+            end
+
+            @peek_line
+        end
+
+        def parse
+            while peek_line
+                parse_sample
+            end
+        end
+
+        def parse_sample
+            mem = expect_mem
+            cpu = expect_cpu
+            loadstat = expect_load
+            pids = expect_pids
+
+            Sample.new( mem, cpu, loadstat, pids )
+        end
+
+        def expect_mem
+            expect_hash_with_header( 'Mem', /[:|,]\s+/ )
+        end
+
+        def expect_cpu
+            expect_hash_with_header( 'CPU:', /\s+/ )
+        end
+
+        def expect_load
+            line = get_line
+            fields = line.split(/\s+/)
+
+            if fields[0] != 'Load' and Fields[1] != 'average:'
+                raise "expect Load average: but got '#{line}'"
+            end
+
+            Load.new( fields[2].to_i, fields[3].to_i, fields[4].to_i )
+        end
+
+        SEP_SPACES = /\s+/
+        def expect_pids
+            line = get_line
+            pid_fields = line.split(SEP_SPACES)
+            if !pid_fields.include?('PID')
+                raise "expect PID but got #{line}"
+            end
+
+            line = peek_line
+
+            pids = []
+            while line and !line.start_with? 'Mem:'
+                line = get_line
+                pid_values = line.split(SEP_SPACES)
+                if pid_values.length != pid_fields.length 
+                    raise "Unmatch field numbers for pid output #{line}"
+                end
+
+                pids.push pid_fields.zip(pid_values).to_h
+            end
+        end
+           
+
+        def expect_hash_with_header(io, header, sep)
+            line = get_line
+            fields = line.split(sep)
+            if fields.shift != header
+                raise "expect #{header} but got '#{line}'"
+            end
+
+            if ( fields.length % 2 ) != 0
+                raise "Format error of '#{line}'"
+            end
+
+            result = {}
+            0.upto( fields.length / 2 - 1 ) do |i|
+                result[ fields[i*2 + 1] ] = fields[i*2]
+            end
+
+            result
+        end
+    end
+end
---
+++ test/parser/test_top.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require File.dirname(__FILE__) + "/../test_helper.rb"
+
+class TestParserTop < Test::Unit::TestCase
+    def test_peek_and_getline
+        sample = <<__DATA__
+Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached
+CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq
+Load average: 2.98 2.96 1.78 1/907 3607
+__DATA__
+        io = StringIO.new sample 
+
+        top = Parser::Top.new(io)
+
+        assertEqual( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
+        assertEqual( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
+        assertEqual( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.get_line )
+        assertEqual( 'CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq', top.get_line )
+        assertEqual( 'Load average: 2.98 2.96 1.78 1/907 3607', top.peek_line )
+        assertEqual( 'Load average: 2.98 2.96 1.78 1/907 3607', top.get_line )
+        assertNil top.peek_line
+        assertNil top.get_line
+    end
+end
