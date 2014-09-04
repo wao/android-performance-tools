ea88e0133af559f0a47aa6122d59c036db8d5c53
Yang Chen <mike.c.yang@gmail.com>
2014-09-04 11:28:44 +0800

    VIM auto commit:Thu 04 Sep 2014 11:28:44 AM CST    command:!testrb test/parser/test_top.rb Run options: -Ilib:test
    # Running tests:
    
    E
    
    Finished tests in 0.005218s, 191.6568 tests/s, 1149.9408 assertions/s.
    
      1) Error:
    TestParserTop#test_peek_and_getline:
    EOFError: end of file reached
        /home/w19816/myprojects/android-performance-tools/lib/parser/top.rb:25:in `readline'
        /home/w19816/myprojects/android-performance-tools/lib/parser/top.rb:25:in `peek_line'
        /home/w19816/myprojects/android-performance-tools/test/parser/test_top.rb:23:in `test_peek_and_getline'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1265:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit/testcase.rb:17:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:940:in `block in _run_suite'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:933:in `map'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:933:in `_run_suite'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:663:in `block in _run_suites'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:661:in `each'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:661:in `_run_suites'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:884:in `_run_anything'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1092:in `run_tests'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1079:in `block in _run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1078:in `each'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1078:in `_run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/minitest/unit.rb:1066:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:27:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:780:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:840:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/lib/ruby/2.1.0/test/unit.rb:844:in `run'
        /home/w19816/.rvm/rubies/ruby-2.1.2/bin/testrb:3:in `<main>'
    
    1 tests, 6 assertions, 0 failures, 1 errors, 0 skips
    
    ruby -v: ruby 2.1.2p95 (2014-05-08 revision 45877) [x86_64-linux]

--- test/parser/test_top.rb
+++ test/parser/test_top.rb
@@ -14,13 +14,13 @@ __DATA__
 
         top = Parser::Top.new(io)
 
-        assertEquals( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
-        assertEqual( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
-        assertEqual( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.get_line )
-        assertEqual( 'CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq', top.get_line )
-        assertEqual( 'Load average: 2.98 2.96 1.78 1/907 3607', top.peek_line )
-        assertEqual( 'Load average: 2.98 2.96 1.78 1/907 3607', top.get_line )
-        assertNil top.peek_line
-        assertNil top.get_line
+        assert_equal( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
+        assert_equal( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.peek_line )
+        assert_equal( 'Mem: 727484K used, 12848K free, 0K shrd, 27448K buff, 195288K cached', top.get_line )
+        assert_equal( 'CPU:  4.3% usr  6.5% sys  0.0% nic 89.1% idle  0.0% io  0.0% irq  0.0% sirq', top.get_line )
+        assert_equal( 'Load average: 2.98 2.96 1.78 1/907 3607', top.peek_line )
+        assert_equal( 'Load average: 2.98 2.96 1.78 1/907 3607', top.get_line )
+        assert_nil top.peek_line
+        assert_nil top.get_line
     end
 end
